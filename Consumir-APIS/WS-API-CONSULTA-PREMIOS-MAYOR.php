95ce44a027d927ffa5c9b348d5456825a9fa4c0e
AlexanderCaballero05 <aacaballero@unah.hn>
2023-10-08 19:47:22 -0600

    agregar pagina

---
+++ Consumir-APIS/WS-API-CONSULTA-PREMIOS-MAYOR.php
@@ -0,0 +1,24 @@
+ <?php
+
+class obtenerEndPointMayor{
+    //funcion para consumir la API premios Loteria Mayor sorteo resien jugado        
+    function obtenerDatos()
+    { 
+    $url = "http://localhost/WS-API-CONSULTA-PREMIOS-MAYOR//ws_getPremiosPorSorteo";//esta esta hardcore corregir
+    $json = file_get_contents($url);
+    $datos = json_decode($json, true);
+  
+    return $datos;
+    
+    } 
+ }
+     //  //funcion para consumir la API premios loteria Mayor a Jugar        
+     //  public function ObtenerPremiosAjugar()
+     //  {
+     //    $url = "http://localhost:8080/WS-API-CONSULTA-PREMIOS-MAYOR/ws_getPremiosSorteoSiguiente";//esta esta hardcore corregir
+     //    $json = file_get_contents($url);
+     //    $datos = json_decode($json, true);
+     //  }
+    
+?>
+          
